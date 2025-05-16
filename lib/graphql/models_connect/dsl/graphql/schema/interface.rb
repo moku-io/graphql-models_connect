0ef5a75b3bb783cfdb1dc8182c38d3f2a12dc73c
Riccardo Agatea <riccardo@moku.io>
2025-05-16 10:39:43 +0200

    make class methods work with newer versions of the graphql-ruby gem

--- lib/graphql/models_connect/dsl/graphql/schema/interface.rb
+++ lib/graphql/models_connect/dsl/graphql/schema/interface.rb
@@ -1,25 +1,27 @@
-module GraphQL
-  class Schema
-    module Interface
-      definition_methods do
-        def model_class new_model_class=nil
-          if new_model_class.nil?
-            @model_class ||= default_model_class
-          else
-            @model_class = new_model_class
-          end
-        end
+require 'active_support/concern'
 
-        def default_model_class
-          raise 'You must set an explicit model for anonymous graphql interfaces' if name.nil?
+monkey_patch = Module.new do
+  extend ActiveSupport::Concern
 
-          default_model_name = name.delete_suffix 'Type'
-
-          raise "No default model found for #{name}" unless const_defined? default_model_name
-
-          const_get default_model_name
-        end
+  class_methods do
+    def model_class new_model_class=nil
+      if new_model_class.nil?
+        @model_class ||= default_model_class
+      else
+        @model_class = new_model_class
       end
+    end
+
+    def default_model_class
+      raise 'You must set an explicit model for anonymous graphql interfaces' if name.nil?
+
+      default_model_name = name.delete_suffix 'Type'
+
+      raise "No default model found for #{name}" unless const_defined? default_model_name
+
+      const_get default_model_name
     end
   end
 end
+
+GraphQL::Schema::Interface.prepend monkey_patch
